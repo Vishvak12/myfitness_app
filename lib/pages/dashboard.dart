4a4eccd60278b5add9da7f1348e107cb33bab7ef
Vishvak12 <106383099+Vishvak12@users.noreply.github.com>
2024-04-08 18:09:09 +0530

    new file:   lib/navbar.dart 	modified:   lib/pages/dashboard.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:myfitness_app/models/category_model.dart';
 
@@ -23,6 +25,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     _getCategories();
+    
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
@@ -118,7 +121,6 @@ class _HomePageState extends State<HomePage> {
           ),
         );
   }
-
   AppBar appBar() {
     return AppBar(
       title: const Text(
